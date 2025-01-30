90a9cdbf44cd949e327301da7317452292af87b0
Alper Tuzcu <alpersinantuzcu@hotmail.com>
2025-01-30 15:20:52 +0100

    revert memory leak hotfix

--- vm/vm.go
+++ vm/vm.go
@@ -375,7 +375,7 @@ var (
 	beforeContext = 1024 << 10
 	afterContext  = 128 << 10
 
-	NoOutputTimeout      = 30 * time.Second // Alper
+	NoOutputTimeout      = 5 * time.Minute
 	tickerPeriod         = 10 * time.Second
 	waitForOutputTimeout = 10 * time.Second
 )
